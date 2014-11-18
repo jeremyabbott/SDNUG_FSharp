d207068629a1c8623c963d0da971bbb76027fb8f
Jeremy Abbott <jeremymabbott@gmail.com>
2014-11-17 23:19:18 -0600

    Additional code for presentation

--- SDNUG_FSharp/Program.fs
+++ SDNUG_FSharp/Program.fs
@@ -1,9 +1,5 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-open Module1
+﻿open Module1
 open Module2
-
 
 [<EntryPoint>]
 let main argv = 
@@ -11,5 +7,4 @@ let main argv =
     printfn "%A" (saySomethingShort "Hello" "World")
     printfn "%A" (sayHelloTo "World")
     printfn "%A" (saySomethingElseShort "Hello" "World")
-    //printCar()
     0 // return an integer exit code
