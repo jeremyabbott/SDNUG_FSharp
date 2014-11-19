a58f972e0fca5c4fac9e0589aeca8610b973e803
Jeremy Abbott <jeremymabbott@gmail.com>
2014-11-19 16:39:03 -0600

    Removed unused code.

--- SDNUG_FSharp/Module7.fs
+++ SDNUG_FSharp/Module7.fs
@@ -1,54 +1,7 @@
 ﻿module Module7
 
     open System
-    //open Microsoft.FSharp.Control  // Async.* is in this module.
 
-    let userTimerWithAsync () = 
-
-        // create a timer and associated async event
-        let timer = new System.Timers.Timer(2000.0)
-        // Timer event will do something when timer.Elapsed fires.
-        let timerEvent = Async.AwaitEvent (timer.Elapsed) |> Async.Ignore
-
-        // start
-        printfn "Waiting for timer at %O" DateTime.Now.TimeOfDay
-        timer.Start()
-
-        // keep working
-        printfn "Doing something useful while waiting for event"
-
-        // process will unblock once timerEvent goes off.
-        Async.RunSynchronously timerEvent
-
-        // done
-        printfn "Timer ticked at %O" DateTime.Now.TimeOfDay
-
-    let countInParallel () =
-        let sleepWorkflow  = async {
-                printfn "Starting child sleep workflow at %O" DateTime.Now.TimeOfDay
-                do! Async.Sleep 2000
-                printfn "Finished child sleep workflow at %O" DateTime.Now.TimeOfDay
-            }
-
-        let nestedWorkflow  = async{
-
-                printfn "Starting parent"
-                let! childWorkflow = Async.StartChild sleepWorkflow
-
-                // give the child a chance and then keep working
-                do! Async.Sleep 100
-                printfn "Parent doing something useful while waiting "
-
-                // block on the child
-                let! result = childWorkflow
-
-                // done
-                printfn "Finished parent" 
-            }
-
-        // run the whole workflow
-        Async.RunSynchronously nestedWorkflow 
-    
     let createAsyncWorkFlow delay name =
         async {
                 printfn "Workflow %s started at %O" name DateTime.Now.TimeOfDay
